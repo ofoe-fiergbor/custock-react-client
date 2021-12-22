d1152f2a4b64fed420fad415f2f1e831edd54b94
Ofoe Fiergbor <ofoefiergbor16@gmail.com>
2021-12-22 00:10:03 +0000

    auth state management in redux

---
+++ src/configurations/domain/redux/Authentication.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  userType: undefined,
+  user: undefined,
+};
+
+const Authentication = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser: (state, action) => (state.user = action.payload),
+    setUserType: (state, action) => (state.userType = action.payload),
+    logout: () => initialState,
+  },
+});
+
+export const { setUser, setUserType, logout } = Authentication.reducer;
+
+export default Authentication;
--- src/configurations/domain/redux/store.js
+++ src/configurations/domain/redux/store.js
@@ -1,5 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
+import Authentication from "./Authentication";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: Authentication,
+  },
 });

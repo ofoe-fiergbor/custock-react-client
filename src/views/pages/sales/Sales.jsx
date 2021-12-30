859956b057dc099deeb3831634a5415c474140ac
Ofoe Fiergbor <ofoefiergbor16@gmail.com>
2021-12-30 20:49:23 +0000

    sales record

--- src/views/pages/sales/Sales.jsx
+++ src/views/pages/sales/Sales.jsx
@@ -1,12 +1,39 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Navbar from "../../components/navbar/Navbar";
+import "./sales.css";
+import dayjs from "dayjs";
 
 const Sales = () => {
+  const { invoices } = useSelector((state) => state.product);
   return (
     <div className="page">
       <Navbar />
-      <div className="inner-ctn">
-        <h1>sale</h1>
+      <div className="sales-inner-ctn">
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">Date</th>
+              <th scope="col">Buyer</th>
+              <th scope="col">Social Media</th>
+              <th scope="col">Product</th>
+              <th scope="col">Quantity</th>
+              <th scope="col">Details</th>
+            </tr>
+          </thead>
+          <tbody>
+            {invoices.map((invoice) => (
+              <tr key={invoice.id}>
+                <td>{dayjs(invoice.timestamp).format("DD/MM/YYYY")}</td>
+                <td>{invoice.buyer}</td>
+                <td>{invoice.socialMedia}</td>
+                <td>{invoice.product?.name}</td>
+                <td>{invoice.quantity}</td>
+                <td>more>></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );

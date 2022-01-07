064798ec47cf64d9ff384a30df3cbc5d622d934d
Ofoe Fiergbor <ofoefiergbor16@gmail.com>
2022-01-07 21:03:58 +0000

    inventory display

--- src/views/pages/stockItems/StockItems.jsx
+++ src/views/pages/stockItems/StockItems.jsx
@@ -1,12 +1,38 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { formatNumbers } from "../../../utils/utils";
 import Navbar from "../../components/navbar/Navbar";
+import "./stockItems.css";
 
 const StockItems = () => {
+  const { products } = useSelector((state) => state.product);
+
   return (
     <div className="page">
       <Navbar />
-      <div className="inner-ctn">
-          <h1>stock items</h1>
+      <div className="stock-inner-ctn">
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">#Id</th>
+              <th scope="col">Name</th>
+              <th scope="col">Balance</th>
+              <th scope="col">Measure</th>
+              <th scope="col">Supplier</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <tr key={product.id}>
+                <th scope="row">{product.id}</th>
+                <td>{product.name}</td>
+                <td>{formatNumbers(product.balance)}</td>
+                <td className="uom">{product.unitOfMeasurement}</td>
+                <td>{product.supplier.name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
